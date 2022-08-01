33652f0f4b283a38e98c3ab4eb6c5d5e19af39fb
Bhavik Dudhrejiya <77838416+BhavikDudhrejiya@users.noreply.github.com>
2022-08-01 23:36:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -70,10 +70,15 @@ if st.button('Recommend'):
     
     for i,j in zip(recommendation, posters):
         cc = next(cols)
-        r = requests.get(j)
-        img = Image.open(BytesIO(r.content))
-        resizedImg = img.resize((250, 325), Image.ANTIALIAS)
-        cc.image(resizedImg, caption=i)
+        try:
+            r = requests.get(j)
+            img = Image.open(BytesIO(r.content))
+            resizedImg = img.resize((250, 325), Image.ANTIALIAS)
+            cc.image(resizedImg, caption=i)
+        except:
+            r = requests.get('https://media.istockphoto.com/vectors/page-not-found-error-with-film-flap-design-vector-id1265221960?k=20&m=1265221960&s=170667a&w=0&h=jCITUlo5a7s5fue3XrX2WB8FOK9VnbaWeLCHB8Ovj-c=')
+            img = Image.open(BytesIO(r.content))
+            cc.image(resizedImg, caption=i)
 #------------------------------------------------------------------------------------------------------------------------
     
 
